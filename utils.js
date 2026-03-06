c63e422743a7410dfb75e41ab920780beca65adf
savasakar <130222546+savasakar@users.noreply.github.com>
2026-03-06 19:48:15 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T16:48:09.371Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-06T16:48:14.633Z
+Commit: Fix CSS styling bug on mobile devices
