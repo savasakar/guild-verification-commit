0ce61ed5258b199ac72da966cd17f8479ef8a9c6
savasakar <130222546+savasakar@users.noreply.github.com>
2026-03-06 20:27:21 +0300

    Implement user profile customization

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T17:27:06.940Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-06T17:27:20.749Z
+Commit: Implement user profile customization
