f2ad01778803c2fff746960346bd9dc2bea64729
savasakar <130222546+savasakar@users.noreply.github.com>
2026-03-06 19:48:34 +0300

    Introduce real-time notifications

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T16:48:12.055Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-06T16:48:33.799Z
+Commit: Introduce real-time notifications
