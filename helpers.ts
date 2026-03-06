d04a4042d84c41ee914eab442a4cc50a9bd5cedd
savasakar <130222546+savasakar@users.noreply.github.com>
2026-03-06 20:27:01 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T17:26:57.957Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-06T17:27:00.441Z
+Commit: Patch API endpoint error handling
