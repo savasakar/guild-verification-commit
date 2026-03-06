27c35423c58f6d6a388937fbf01b5343f7d7e79e
savasakar <130222546+savasakar@users.noreply.github.com>
2026-03-06 19:48:27 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T16:48:06.651Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-06T16:48:27.083Z
+Commit: Fix memory leak in event handlers
