c28354cf4cebf53989339db899d651b4496bddce
savasakar <130222546+savasakar@users.noreply.github.com>
2026-03-06 20:26:59 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T17:26:56.635Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T17:26:59.175Z
+Commit: Fix CSS styling bug on mobile devices
