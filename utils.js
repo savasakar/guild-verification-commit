3ed06d19e893b95e65c950f5ac23a8b4497b8673
savasakar <130222546+savasakar@users.noreply.github.com>
2026-03-06 20:26:57 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T17:26:51.233Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-06T17:26:56.635Z
+Commit: Fix broken links in documentation
