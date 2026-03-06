08105beb2bce4bcdf6369ac8b935a53128e6be22
savasakar <130222546+savasakar@users.noreply.github.com>
2026-03-06 19:48:07 +0300

    Resolve timezone issues in date handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T16:47:57.271Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T16:48:06.651Z
+Commit: Resolve timezone issues in date handling
