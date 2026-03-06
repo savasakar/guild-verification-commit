e38968251ca83c0a7ff209d33eae65a32a84b746
savasakar <130222546+savasakar@users.noreply.github.com>
2026-03-06 20:27:07 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T17:27:05.729Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-06T17:27:06.940Z
+Commit: Resolve null pointer exception in user service
