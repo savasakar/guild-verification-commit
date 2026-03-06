3674115a73d83cc2aa4cac7bf8ce121e66f0f44a
savasakar <130222546+savasakar@users.noreply.github.com>
2026-03-06 20:27:06 +0300

    Introduce real-time notifications

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T17:27:00.441Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-06T17:27:05.729Z
+Commit: Introduce real-time notifications
