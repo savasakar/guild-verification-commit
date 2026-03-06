7542e1106800c8559e135c696bd58241e025513a
savasakar <130222546+savasakar@users.noreply.github.com>
2026-03-06 19:48:46 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T16:48:31.156Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-06T16:48:45.877Z
+Commit: Fix broken links in documentation
