de99a409fb177c0143be3f0e0fbabd1843e81973
savasakar <130222546+savasakar@users.noreply.github.com>
2026-03-06 20:27:19 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T17:27:15.851Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-06T17:27:18.286Z
+Commit: Fix CSS styling bug on mobile devices
